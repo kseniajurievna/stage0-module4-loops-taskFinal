2195c5c16d43704e4ea00503302bf1962bf6727f
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-23 14:20:43 -0800

    new1

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 0; i <=cathetusLength-1; i ++){
+            for (int j = 2*(cathetusLength-i); j >=0; j--){
+                System.out.print(" ");
+            }
+            for (int n = 0; n <=i; n++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        int sum = 0;
+        for (int i = 1; i <=10; i++){
+            sum = numberTableToPrint * i;
+            System.out.println(numberTableToPrint + "*" + i + "=" + sum);
+        }
     }
 }
